88b2a58959a6049f10629ac4a0eb071ff83aeb19
noah nichols <noahjacknichols@gmail.com>
2020-05-03 17:03:39 -0400

    added cards

--- declr/src/Description.js
+++ declr/src/Description.js
@@ -1,25 +1,58 @@
 
 import React, { Component } from 'react';
-import {Grid, Paper} from '@material-ui/core';
+import {Grid, Paper, CardContent, CardActions, Button, CardActionArea, CardMedia,  Card, Typography} from '@material-ui/core';
+import { sizing } from '@material-ui/system'
 
 class Description extends Component {
     render(){
         return (
-            <div>
-                <Grid container direction = "column" justify="center" alignItems="center" spacing={3}>
-                    <Grid item xs ={12}>
-                        <Paper xs ={12}>
-                        <div> this is a test</div>
-                        </Paper>
+
+            <div style = {{padding: 20}}>
+                <Grid container direction = "column" justify="center" spacing = {4}>
+                    <Grid item  xs={12} >
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia
+                                    wide
+                                    component="img"
+                                    alt="card img"
+                                    height = "140"
+                                    image="./images/cards/yoda.jpg"
+                                    title="img1"/>
+                            
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Project 1
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    this is a test of my project 1 body
+                                </Typography>
+                            </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Github
+                                </Button>
+                            </CardActions>
+                        </Card>
                     </Grid>
                     <Grid item xs ={12}>
-                        <div> this is a test</div>
+                        <Card>
+                            <CardContent>
+                                <div> this is a test</div>
+                            </CardContent>
+                        </Card>
                     </Grid>
                     <Grid item xs ={12}>
-                        <div> this is a test</div>
+                        <Card>
+                            <CardContent>
+                                <div> this is a test</div>
+                            </CardContent>
+                        </Card>
                     </Grid>
+
                 </Grid>
-            </div>
+            </div> 
 
 
         );
--- declr/src/Home.js
+++ declr/src/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     render(){
         return (
             <Box>
-            <Grid container direction = "row" justify = "flex-start" alignItems="stretch" spacing={1}>
+            <Grid container direction = "row" justify = "flex-start" alignItems="stretch" spacing={0}>
                 <Grid item xs = {4} alignItems="stretch">
                     <Paper >xs</Paper>
                 </Grid>
