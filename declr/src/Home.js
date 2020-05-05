0e268cd296471ec9329000a4f6f7e4541600a269
noahjacknichols <noahjacknichols@gmail.com>
2020-05-05 00:26:59 -0400

    changed project descriptions and some styling

--- declr/src/Home.js
+++ declr/src/Home.js
@@ -4,19 +4,22 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Paper, Box } from '@material-ui/core';
-import Description from './Description';
+import Projects from './Projects';
 
 
 class Home extends Component {
     render(){
         return (
+            
             <Box>
-            <Grid container direction = "row" justify = "flex-start" alignItems="stretch" spacing={0}>
-                <Grid item xs = {4} alignItems="stretch">
-                    <Paper >xs</Paper>
+            <Grid container direction = "row" justify = "flex-start" alignItems="stretch" spacing={0} >
+                <Grid item xs = {4} alignItems="stretch" style={{backgroundColor: '#414141', height:'100vh'}}>
+                    <div>
+                        my info goes here
+                    </div>
                 </Grid>
-                <Grid item xs={8} alignItems="stretch">
-                    <Description/>
+                <Grid item xs={8} alignItems="stretch" style={{backgroundColor: '#313131'}}>
+                    <Projects/>
                 </Grid>
                 
             </Grid>
--- declr/src/Projects.js
+++ declr/src/Projects.js
@@ -1,36 +1,37 @@
 
 import React, { Component } from 'react';
-import {Grid, Paper, CardContent, CardActions, Button, CardActionArea, CardMedia,  Card, Typography} from '@material-ui/core';
+import {Grid, Paper, CardContent, CardActions, Button, CardActionArea, CardMedia,  Card, Typography, Icon} from '@material-ui/core';
 import { sizing } from '@material-ui/system'
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 class Description extends Component {
     render(){
         return (
 
-            <div style = {{padding: 20}}>
-                <Grid container direction = "column" justify="center" spacing = {4}>
-                    <Grid item  xs={12} >
+            <div style = {{ padding: 20}}>
+                <Grid container direction = "column" alignItems="center" spacing = {4} >
+                    <Grid item  xs={8} justify="center" style={{minWidth:'55vw'}}>
                         <Card>
                             <CardActionArea>
                                 <CardMedia
                                     wide
                                     component="img"
                                     alt="card img"
-                                    height = "140"
-                                    image="./images/cards/yoda.jpg"
+                                    height = "200"
+                                    image= {require('./images/cards/img1.gif')}
                                     title="img1"/>
                             
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    Project 1
+                                    Java Compiler/Interpreter
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
-                                    this is a test of my project 1 body
+                                    this is a test of my project 1 body 
                                 </Typography>
                             </CardContent>
                             </CardActionArea>
                             <CardActions>
-                                <Button size="small" color="primary">
+                                <Button variant="contained" color="default" startIcon={<GitHubIcon/>} href ="https://github.com/noahjacknichols/Compiler">
                                     Github
                                 </Button>
                             </CardActions>
